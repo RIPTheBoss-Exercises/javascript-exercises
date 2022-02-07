75ec3367c78ad3026cb3962e43beec83e8bd5dbf
RIPTheBoss <hugo.delautre.alt@gmail.com>
2022-02-07 15:53:12 +0100

    exo done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let strArray = Array.from(str);
 
+    strArray = strArray.filter(function(letter){
+        if(letter === "!" ||
+        letter === ":" ||
+        letter === ";" ||
+        letter === "," ||
+        letter === "?" ||
+        letter === "." ||
+        letter === " "){
+            return false;
+        }else{
+            return true;
+        }
+    })
+
+    let uno = strArray.toString().toLowerCase();
+    let dos = strArray.reverse().toString().toLowerCase();
+
+    return uno === dos ? true : false;
 };
 
 // Do not edit below this line
