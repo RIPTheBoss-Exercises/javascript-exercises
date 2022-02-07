dc83a606bbbd33598bc7b867712f2e8b004bfeab
RIPTheBoss <hugo.delautre.alt@gmail.com>
2022-02-07 13:08:47 +0100

    Exo done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,43 @@
-const add = function() {
-	
+const add = function(...numbers) {
+  return numbers.reduce(function(total, currentValue){
+    return total + currentValue;
+  })
 };
 
-const subtract = function() {
-	
+const subtract = function(...numbers) {
+	return numbers.reduce(function(total, currentValue){
+    return total - currentValue;
+  })
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce(function(total, currentValue){
+    return total + currentValue;
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce(function(total, currentValue){
+    return total * currentValue;
+  }, 1)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  let result = 1;
+  for(let i = 0; i<b; i++){
+    result *= a;
+  }
+	return result;
 };
 
-const factorial = function() {
+const factorial = function(num) {
+  if(num == 0) {
+    return 1;
+  }else if(num < 0){
+    return -1;
+  }else{
+    return num*factorial(num-1);
+  }
 	
 };
 
