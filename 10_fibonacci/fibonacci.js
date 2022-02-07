1bf51ae4090bde98cfe20d01619e480256974f0b
RIPTheBoss <hugo.delautre.alt@gmail.com>
2022-02-07 16:21:37 +0100

    exo done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if(index<0){
+        return "OOPS";
+    }
+    
+    let fibonaci = [1,1];
 
+    while(fibonaci.length<index){
+    fibonaci.push((fibonaci[fibonaci.length-1]
+     +fibonaci[fibonaci.length-2]));
+    }
+
+    return fibonaci[index -1];
 };
 
 // Do not edit below this line
