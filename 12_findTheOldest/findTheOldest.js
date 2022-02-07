f60f6c1b26bd5bb662158f929a8b7e3cbc4ed7a4
RIPTheBoss <hugo.delautre.alt@gmail.com>
2022-02-07 16:43:26 +0100

    exo done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(persons) {
 
+    const age = function(someone){
+        if(someone.yearOfDeath !== undefined){
+            return someone.yearOfDeath - someone.yearOfBirth;
+        }else{
+            return new Date().getFullYear() - someone.yearOfBirth;
+        }
+    }
+
+    persons = persons.sort((a,b) => age(a) > age(b) ? 1:-1); //Order persons from youngest to oldest
+
+    return persons[(persons.length - 1)];
 };
 
 // Do not edit below this line
